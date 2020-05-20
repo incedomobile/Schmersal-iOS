37e05d4c22c2fad4360fac629bd9f3791c3cc5e3
Ankit Manglik <amanglik@psft.com>
2020-05-20 17:08:52 +0530

    Initial Commit

---
+++ Schmersal/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Schmersal
+//
+//  Created by Ankit on 20/05/20.
+//  Copyright © 2020 Ankit. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
